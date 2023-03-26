15cc51361f1b68b5d6486db9023f5f65180db48f
Lori <lcenos5@gmail.com>
2023-03-25 20:46:40 -0500

    Candle Sales SQL

---
+++ candle-sales/CANDLES DATA.sql
@@ -0,0 +1,11 @@
+-- Customers
+INSERT INTO customers (customer_id, first_name, last_name, phone) VALUES('CRUISE_TOM', 'Tom', 'Cruise', '479.223.5969');
+INSERT INTO customers (customer_id, first_name, last_name, phone) VALUES('MCCOLLUM_PARKER', 'Parker', 'McCollum', '415.399.4665');
+INSERT INTO customers (customer_id, first_name, last_name, phone) VALUES('WITHERSPOON_REESE', 'Reese', 'Witherspoon', '415.447.7583');
+INSERT INTO customers (customer_id, first_name, last_name, phone) VALUES('STEFANI_GWEN', 'Gwen', 'Stefani', '209.993.3774');
+
+
+-- Options
+INSERT INTO options (option_pk, candle_type, scent, candle_size, price) VALUES('1', 'JAR', 'VANILLA', 'SMALL', '10.99');
+INSERT INTO options (option_pk, candle_type, scent, candle_size, price) VALUES('2', 'THREE_WICK', 'VANILLA', 'MEDIUM', '5.99');
+INSERT INTO options (option_pk, candle_type, scent, candle_size, price) VALUES('3', 'TUMBLER', 'VANILLA', 'LARGE', '20.99');
